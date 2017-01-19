d102c09619905df9a15dc53200787a7a7f35a71a
zaid <carloszaid@gmail.com>
2017-01-18 17:06:27 -0700

    css finalizado e inicio javasscript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// TODO agregar condigo javascript
+//alert("Hola Mundo");
+
+console.log('Hola desde la consola de js');
+function login()
+{
+    var usuario = document.getElementById('txtusuario').value;
+    var password = document.getElementById('txtpassword').value;
+    alert('usuario:'+ usuario +' Contraseña: '+password);
+}
