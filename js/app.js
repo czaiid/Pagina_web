e2b7d48a5879395537bd10aa7d61226045ea7b50
zaid <carloszaid@gmail.com>
2017-02-01 17:08:07 -0700

    perfil.html, sessionstorage y validacion en index

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 
 function login(usuario,password){
-    $.post("login.php", { txtusuario:usuario, txtpassword:password }, function(data){
-        return data;
+    $.post("login.php", { txtusuario:usuario, txtpassword:password }, function(response){
+        var acceso = response.data.login;
+        if(acceso == true){
+            var usuario = response.data.usuario;
+            sessionStorage.userid = usuario.id;
+            sessionStorage.username = usuario.username;
+            sessionStorage.nombre = usuario.nombre;
+            sessionStorage.apellidos = usuario.apellidos;
+            sessionStorage.email = usuario.email;
+            console.log(usuario);
+        }else if (acceso == false){
+            $('#login-form-msg').html('Usuario o contraseña incorrecta');
+            $('#login-form-msg').removeClass('oculto');
+            $('#txtpassword').val('');
+        }else{
+            $('#login-form-msg').html('Error en la conexion a la base de datos');
+            $('#login-form-msg').removeClass('oculto');
+        }
     });
 }
 
@@ -10,6 +26,20 @@ $(document).ready(function(){
         event.preventDefault();
         var usuario = $('#txtusuario').val();
         var password = $('#txtpassword').val();
-        login(usuario,password);
+        if (usuario ==''){
+            //quitar la clase oculto del div de error de txtusuario
+            $('#txtusuario+div').removeClass('oculto');
+        }else{
+            $('#txtusuario+div').addClass('oculto');
+        }
+        if (password==''){
+            //quitar la clase oculto del div de error de txtpassword
+            $('#txtpassword+div').removeClass('oculto');
+        }else{
+            $('#txtpassword+div').addClass('oculto');
+        }
+        if(usuario != '' && password !=''){
+            login(usuario,password);
+        }
     });
 });
